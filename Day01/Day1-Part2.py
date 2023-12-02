e67c495decf847d811ccf8722227adf03b400aa2
CyberF0x <CyberF0x>
2023-12-02 03:38:07 +0000

    removed str calls as not needed

--- Day01/Day1-Part2.py
+++ Day01/Day1-Part2.py
@@ -8,20 +8,18 @@ What is the sum of all the calibration values
 
 """
 
-
 ints = ["0","1","2","3","4","5","6","7","8","9"]
 
 mapping = {
-    "one":1,
-    "two":2,
-    "three":3,
-    "four":4,
-    "five":5,
-    "six":6,
-    "seven":7,
-    "eight":8,
-    "nine":9
-    
+    "one":"1",
+    "two":"2",
+    "three":"3",
+    "four":"4",
+    "five":"5",
+    "six":"6",
+    "seven":"7",
+    "eight":"8",
+    "nine":"9"    
 }
 
 
@@ -32,46 +30,37 @@ def main():
             acc = acc + get_ints_from_cal(each_line.lower().rstrip())
     print(acc)
     
+    
 def get_ints_from_cal(cal_val):
     first = None
     last = None
     # Easy checks first
-    if str(cal_val[0]) in ints :
-        first = str(cal_val[0])
-        
-    if str(cal_val[-1]) in ints :
-        last = str(cal_val[-1])
-    
+    if cal_val[0] in ints :
+        first = cal_val[0]
+    if cal_val[-1] in ints :
+        last = cal_val[-1]
     # Otherwise put effort in    
     indexes = []
-    
     for each_key in mapping:
-        if str(each_key) in cal_val:
-            indexes.append((cal_val.index(each_key),str(mapping[each_key])))
-            indexes.append((cal_val.rindex(each_key),str(mapping[each_key])))
-    filtered_cal = [eval(i) for i in list(filter(filter_ints,cal_val))]    
+        if each_key in cal_val:
+            indexes.append((cal_val.index(each_key),mapping[each_key]))
+            indexes.append((cal_val.rindex(each_key),mapping[each_key]))
+    filtered_cal = [i for i in list(filter(filter_ints,cal_val))]    
     for each_int in filtered_cal:
-        indexes.append((cal_val.index(str(each_int)),str(each_int)))
-        indexes.append((cal_val.rindex(str(each_int)),str(each_int)))
-        
+        indexes.append((cal_val.index(each_int),each_int))
+        indexes.append((cal_val.rindex(each_int),each_int))
     indexes = sorted(indexes)
     if first == None:
         first = indexes[0][1]
     if last == None:
         last = indexes[-1][1]
-    
     combined = first + last
-    
-    print(combined)
-    
-    
     return int(combined)
-    
-    
-    
+ 
+     
 def filter_ints(cal_val):
     return True if cal_val in ints else False
   
+  
 if __name__ == "__main__":
- 
-    main()
+     main()
